744a72f45cc82c56401371e687207e11e5b6ad7d
Jennifer Paquette <jennidemuir@gmail.com>
2020-04-15 00:55:23 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,10 +1,8 @@
 # Write your code here
 count = 10 
-magic_number = 0 
+
 while count >= 1 do
   puts count
 count -= 1 
 end
-magic_number do
-  puts "Happy New Year!"
-end
+puts "Happy New Year!"
